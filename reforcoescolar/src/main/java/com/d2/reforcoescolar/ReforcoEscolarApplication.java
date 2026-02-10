46cd49848ab6aebb2242d393aafecc1d4f254eca
Ediene <edienegomes44@gmail.com>
2026-02-10 00:10:48 -0300

    Iniciando projeto SpringBoot

---
+++ reforcoescolar/src/main/java/com/d2/reforcoescolar/ReforcoEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.d2.reforcoescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReforcoEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReforcoEscolarApplication.class, args);
+	}
+
+}
---
+++ reforcoescolar/src/test/java/com/d2/reforcoescolar/ReforcoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d2.reforcoescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReforcoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
